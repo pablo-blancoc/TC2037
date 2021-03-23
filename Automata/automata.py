52eeb2f6ec8e5b77e55a8cf29419cdbc72c26bed
pablo-blancoc <pablo_blancoc@outlook.es>
2021-03-22 18:15:05 -0600

    se agrego la tabla, se empezo automata.py

--- Automata/automata.py
+++ Automata/automata.py
@@ -1,2 +1,28 @@
-print("funciona")
+def analyze(line: str):
+    """Analiza cada una de las lineas del archivo
 
+    Args:
+        line (str): La línea a analizar
+    """
+    print(line)
+
+
+def lexerAritmetico(archivo: str):
+    """
+    Función principal donde se lee el archivo y se mandan a llamar las demás funciones para realizar el procesamiento
+
+    Args:
+        archivo (str): Nombre del archivo que se tiene que leer
+    """
+    try:
+        with open(archivo, 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            analyze(line)
+            
+    except FileNotFoundError:
+        print(f'ERROR: El archivo "{archivo}" no se ha encontrado.')
+    
+    
+file = input("NOMBRE DEL ARCHIVO: ")
+lexerAritmetico(archivo=file)
