58c09c1b0989ee38e934b74bc851f932ec0eaead
pablo-blancoc <pablo_blancoc@outlook.es>
2021-03-25 16:26:34 -0600

    minor bug fix

--- DFA/install-mac.sh
+++ DFA/install-mac.sh
@@ -9,6 +9,7 @@ pip3 install -r requirements.txt
 curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/tabla.csv > tabla.csv
 curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/automata.py > automata.py
 curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/README.txt > README.txt
+deactivate
 clear
 echo "============================================"
 echo "============================================"
