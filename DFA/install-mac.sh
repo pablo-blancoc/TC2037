53c13a20e6c38bf3a02e718f4c60e498ec7c4ad7
pablo-blancoc <pablo_blancoc@outlook.es>
2021-03-25 16:05:24 -0600

    updated install-mac.sh

--- DFA/install-mac.sh
+++ DFA/install-mac.sh
@@ -8,6 +8,7 @@ curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/requireme
 pip3 install -r requirements.txt
 curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/tabla.csv > tabla.csv
 curl https://raw.githubusercontent.com/pablo-blancoc/TC2037/master/DFA/automata.py > automata.py
+clear
 echo "============================================"
 echo "============================================"
 echo ""
@@ -21,3 +22,4 @@ echo ""
 echo ""
 echo "Todo listo! Diviertete!!"
 echo "-- Brucelords"
+echo ""
