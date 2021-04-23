57f118a5a476324e2bfaf19c388871d743dd678a
pablo-blancoc <pablo_blancoc@outlook.es>
2021-04-23 15:30:48 -0500

    started sintaxis analysis

---
+++ Sintaxis/Test.scm
@@ -0,0 +1,13 @@
+(define (name param1-param2) ; (, define, (, variable, variable, operador, variable, ), ), comentario
+    body ;   aquí;   ocurre   la-mágia - variable, comentario
+) ; (, comentario
+
+(name 'a) ; (, variable, símbolo, comentario
+
+
+'name+1  -name*3^4/5.2 ; símbolo, operador, variable, operador, número, operador, número, operador, número, comentario
+
+#t#f ; error, comentario
+1.2ebc+#f ; error, operador, falso, comentario
+'(#t+#f) ; símbolo, comentario
+hol@2-mundo2 ; error, operador, variable, comentario
