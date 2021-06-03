06b0a7fbb1ba8609702f34ce98b443760b077afc
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2021-06-03 10:47:32 -0500

    ConsumerThread
    Se agregaron las variables que se pueden modificar

--- Actividades/5.3/main.py
+++ Actividades/5.3/main.py
@@ -38,3 +38,52 @@ class ProducerThread(threading.Thread):
             if os.path.isdir(os.path.join(path, file)):
                 continue
             yield os.path.join(path, file)
+
+class ConsumerThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, verbose=None):
+        super(ConsumerThread,self).__init__()
+        self.target = target
+        self.name = name
+        return
+
+    def run(self):
+        while True:
+            if not q.empty():
+                item = q.get()
+                if item is _sentinel:
+                    q.put(_sentinel)
+                    break
+                else:
+                    logging.debug('Getting ' + str(item).split('/')[-1] + ' : ' + str(q.qsize()) + ' items in queue')
+                    # Procesar el archivo
+                    x = item.split("/")
+                    x[-1] = x[-1].split(".")[0] + ".html"
+                    salida = '/'.join(x)
+                    resaltadorLexico(archivo=item, salida=salida)
+        return
+
+
+BUF_SIZE = 10
+N = 10
+PATH = "/Users/Pablo/Library/Mobile Documents/com~apple~CloudDocs/ITC/Semestre_4/MetodosComputacionales/repo/Actividades/5.3/test"
+q = queue.Queue(BUF_SIZE)
+    
+
+if __name__ == '__main__':
+    start_time = time.time()
+    p = ProducerThread(name='producer', path=PATH)
+    p.start()
+    c = []
+    for i in range(N):
+        c.append(ConsumerThread(name=str(i)))
+        c[i].start()
+        
+    while True:
+        if p.done:
+            p.join()
+            for consumer in c:
+                consumer.join()
+            break
+    
+    print(f"No. hilos: {N} - Tiempo: {round(time.time() - start_time, 5)} seg.")
